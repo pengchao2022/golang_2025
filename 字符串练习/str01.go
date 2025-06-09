1103b9025a9190926fc6a125f4545bf754570d43
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-06-10 07:32:58 +0800

    added

--- 字符串练习/str01.go
+++ 字符串练习/str01.go
@@ -21,4 +21,46 @@ func main() {
 	name2 := " ma"
 	name := name1 + name2
 	fmt.Println(name)
+
+	/*
+		转义符练习
+	*/
+
+	name_list := "allen\nkate\nlily\n"
+	fmt.Println(name_list)
+
+	/*
+		双引号的打印
+	*/
+	qu_str := "Hello, this is \"jack\""
+	fmt.Println(qu_str)
+
+	/*
+		文件路径的打印
+	*/
+	my_path := "\\d\\work\\next"
+	fmt.Println(my_path)
+
+	/*
+		多行内容的打印 使用反引号
+	*/
+	my_words := `
+	春眠不觉晓
+	   处处闻啼鸟
+	      夜来风雨声
+		     花落知多少
+	
+	`
+	fmt.Println(my_words)
+
+	customer_choose := `
+	1,添加客户信息
+	2,删除客户信息
+	3,查询客户信息
+	4,修改客户信息
+	5,退出系统
+	
+	`
+	fmt.Println(customer_choose)
+
 }
