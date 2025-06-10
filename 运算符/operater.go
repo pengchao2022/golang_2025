841221ac3300c5c553d65291d758a60b252a348d
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-06-10 10:48:57 +0800

    added

---
+++ 运算符/operater.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 取余数运算符 %
+	a := 7
+	b := 2
+	c := a % b
+	fmt.Println(c)
+
+	// 关系运算符 返回一个bool布尔值
+	fmt.Println(a > b)
+	fmt.Println(a != b)
+	fmt.Println(a <= b)
+
+	//逻辑运算符
+	username := "allen"
+	passwd := "12378965"
+
+	fmt.Println(username == "allen" && passwd == "12378965")
+	fmt.Println("可以登录")
+
+}
