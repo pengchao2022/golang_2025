e9356c8d8effad2f4ad065b1dd43b497db7021f8
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-06-13 00:08:55 +0800

    added

---
+++ 函数基础/function1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// 自定义自己的一个函数
+// 函数声明不会执行内部的代码
+// 函数作用是包裹 或者 封装
+// 函数是代码的一种组织形式
+func loop_print() {
+	for i := 1; i <= 100; i++ {
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	//自定义的函数，需要放在main()函数里去执行
+	loop_print()
+
+}
